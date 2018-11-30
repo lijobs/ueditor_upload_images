2bdfa725471dfd5456bb083bbc92c0c0514fd4a1
李泽华 <466974280@qq.com>
2018-11-30 10:50:44 +0800

    springboot整合百度符文卡u那个编辑器ueditor的图片上传和回显功能

---
+++ src/main/java/com/example/demo/define/ActionState.java
@@ -0,0 +1,5 @@
+package com.example.demo.define;
+
+public enum ActionState {
+	UNKNOW_ERROR
+}
---
+++ src/main/java/com/example/demo/example/UEditorController.java
@@ -0,0 +1,40 @@
+package com.example.demo.example;
+
+import com.example.demo.ActionEnter;
+import org.json.JSONException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ldb on 2017/4/9.
+ */
+@Controller
+public class UEditorController {
+
+
+    @RequestMapping("/")
+    private String showPage(){
+        return "index";
+    }
+
+    @RequestMapping(value="/config")
+    public void config(HttpServletRequest request, HttpServletResponse response) throws JSONException {
+        response.setContentType("application/json");
+        String rootPath = request.getSession().getServletContext().getRealPath("/");
+        try {
+            String exec = new ActionEnter(request, rootPath).exec();
+            PrintWriter writer = response.getWriter();
+            writer.write(exec);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
